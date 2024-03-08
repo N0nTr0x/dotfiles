cec3b961a2518e96894c625210c68a16dce2b637
Leon <homelab@tech-mueller.de>
2024-03-09 00:24:14 +0100

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -302,12 +302,3 @@ Import-Module "C:\Users\leon\AppData\Local\PowerToys\WinUI3Apps\..\WinGetCommand
 
 # Clear Screen
 Clear-Host
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-    Import-Module "$ChocolateyProfile"
-}
